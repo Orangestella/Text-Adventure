1d25015c1351965c82c19a50df920f2669a81471
Edgar <hou.guanyu@student.zy.cdut.edu.cn>
2023-11-06 14:19:18 +0800

    execise2 1

---
+++ GameObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Edgar on 2023/11/6.
+//
+
+#include "GameObject.h"
---
+++ GameObject.h
@@ -0,0 +1,16 @@
+//
+// Created by Edgar on 2023/11/6.
+//
+
+#ifndef TEXTADV_GAMEOBJECT_H
+#define TEXTADV_GAMEOBJECT_H
+
+
+
+class GameObject {
+
+};
+
+
+
+#endif //TEXTADV_GAMEOBJECT_H
